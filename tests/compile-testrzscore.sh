75cb78dd8233cc414d24bfdba9bf4a475399881f
Zach Carlson <FxChiP@Gmail.com>
2013-12-29 22:06:58 -0800

    oops -- I do want to commit and push test-*.c
    but I also don't want to submit my test binaries. So
    compile test binaries as bin-test-* and leave source
    as test-*. There is probably a more sane way to do
    just this, but I don't want to look into it right now.

--- tests/compile-testrzscore.sh
+++ tests/compile-testrzscore.sh
@@ -1,1 +1,1 @@
-gcc -pedantic -Wall -lusb -I/usr/include/libusb-1.0 -o test-rzscore ../src/rzscore.c ./test-rzscore.c -I../src
+gcc -pedantic -Wall -lusb -I/usr/include/libusb-1.0 -o bin-test-rzscore ../src/rzscore.c ./test-rzscore.c -I../src
